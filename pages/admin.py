5c69170272d254d82cc4cde70912f347c886dc3a
Iman Goudarzvand <im.goudarz@gmail.com>
2022-12-25 23:52:30 +0330

    built admin panel  for team members

--- pages/admin.py
+++ pages/admin.py
@@ -4,16 +4,21 @@ from django.utils.html import format_html
 
 @admin.register(Team)
 class TeamAdmin(admin.ModelAdmin):
-    list_display = ['first_name', 'last_name', 'designation']
+    list_display = ['id', 'thumbnail','first_name' ,'last_name', 'designation']
     readonly_fields = ['thumbnail']
+    list_display_links = ['id', 'thumbnail','first_name']
+    search_fields = ['first_name', 'last_name', 'designation']
+    list_filter = ['designation']
+
 
     def thumbnail(self, instance):
         if instance.image.name != '':
             return format_html(f'<img src="{instance.image.url}" class="thumbnail"/>')
         return ""
 
+    thumbnail.short_description = 'photo'
     class Media:
         css = {
-            'all': ['css/thumbnail.css']
+            'all': ['css/thumbnail2.css']
         }
     
