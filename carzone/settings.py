ed0ac23db74aeea386f9a26d83ec5ee27be6295c
Iman Goudarzvand <im.goudarz@gmail.com>
2023-01-31 11:01:44 +0330

    install allauth  for facebook and google auth

--- carzone/settings.py
+++ carzone/settings.py
@@ -39,9 +39,17 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.facebook',
+    'allauth.socialaccount.providers.google',
+
     'ckeditor',
     'pages',
     'cars',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -136,3 +144,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
+
+SITE_ID = 1
