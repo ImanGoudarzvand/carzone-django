4f4afec5d04db70bbb4efa1e6ec2f31574c1df9b
Iman Goudarzvand <im.goudarz@gmail.com>
2023-02-17 13:22:35 +0330

    dev.py added

---
+++ carzone/settings/dev.py
@@ -0,0 +1,18 @@
+from .common import *
+
+DEBUG = True
+
+SECRET_KEY = 'django-insecure-5q4bs#@gpkqt%coi=t%-owjcp5q#z@kay!hb+fp%8aft_s_m_@'
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+    }
+}
+
+
